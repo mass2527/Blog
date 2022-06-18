c2eb35588c2aa24d28e63506cdf7c672d33991c0
mass2527 <nzp01@naver.com>
2022-06-18 17:30:33 +0900

    feat: create flex style utils function

---
+++ styles/utils/flex.ts
@@ -0,0 +1,24 @@
+import { css, CSSProperties } from 'styled-components';
+
+const flex = (
+  flexDirection: CSSProperties['flexDirection'],
+  justifyContent: CSSProperties['justifyContent'] = 'normal',
+  alignItems: CSSProperties['alignItems'] = 'normal'
+) => {
+  return css`
+    display: flex;
+    flex-direction: ${flexDirection};
+    justify-content: ${justifyContent};
+    align-items: ${alignItems};
+  `;
+};
+
+export const flexRow = (
+  justifyContent?: CSSProperties['justifyContent'],
+  alignItems?: CSSProperties['alignItems']
+) => flex('row', justifyContent, alignItems);
+
+export const flexColumn = (
+  justifyContent?: CSSProperties['justifyContent'],
+  alignItems?: CSSProperties['alignItems']
+) => flex('column', justifyContent, alignItems);
