2ff876f02f853a18be45353b083eb046fda7e84c
mass2527 <nzp01@naver.com>
2022-07-04 23:24:52 +0900

    feat: change main layout

--- src/layouts/GlobalLayout/GlobalMain.tsx
+++ src/layouts/GlobalLayout/GlobalMain.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
 import styled from "styled-components";
+
+import { ReactNode } from "react";
 
 function GlobalMain({ children }: { children: ReactNode }) {
   return <Main>{children}</Main>;
@@ -7,6 +8,19 @@ function GlobalMain({ children }: { children: ReactNode }) {
 
 const Main = styled.main`
   min-height: calc(100vh - 65px);
+
+  display: grid;
+  grid-template-columns:
+    1fr min(
+      ${({ theme }) => theme.sizes.maxWidth},
+      calc(100% - ${({ theme }) => theme.spacers[16]} * 2)
+    )
+    1fr;
+  column-gap: ${({ theme }) => theme.spacers[16]};
+
+  & > * {
+    grid-column: 2;
+  }
 `;
 
 export default GlobalMain;
