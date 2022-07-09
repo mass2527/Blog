8a7babb0497f5a16f0422475764aa8d7251a369f
mass2527 <nzp01@naver.com>
2022-07-09 14:22:45 +0900

    feat: add ResponsiveIFrame component

---
+++ src/components/ResponsiveIFrame.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+import React, { IframeHTMLAttributes } from "react";
+
+interface ResponsiveIFrameProps
+  extends IframeHTMLAttributes<HTMLIFrameElement> {}
+
+function ResponsiveIFrame({
+  allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+  allowFullScreen = true,
+  ...props
+}: ResponsiveIFrameProps) {
+  return (
+    <IFrameWrapper>
+      <iframe allow={allow} allowFullScreen={allowFullScreen} {...props} />
+    </IFrameWrapper>
+  );
+}
+
+const IFrameWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+
+  iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+`;
+
+export default ResponsiveIFrame;
