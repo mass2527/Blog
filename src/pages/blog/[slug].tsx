1c8e5fb0a97c3fa923538299a5af1e84e0f195c8
mass2527 <nzp01@naver.com>
2022-08-06 11:23:00 +0900

    feat: 링크 기능 추가

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -68,6 +68,32 @@ const BlogPost = ({
 
               return <code ref={triggerRef} {...props} />;
             },
+            RegisterLink: ({ id, index, href }) => {
+              const isExternal = href.startsWith("http");
+
+              useEffect(() => {
+                const codeBlock = document.getElementById(id);
+                if (!codeBlock) return;
+
+                const allHighlightWords =
+                  codeBlock.querySelectorAll(".highlight-word");
+                const target = allHighlightWords[index - 1];
+                if (!target) return;
+
+                target.replaceWith(
+                  Object.assign(document.createElement("a"), {
+                    href,
+                    innerHTML: target.innerHTML,
+                    className: target.className,
+                    ...(isExternal
+                      ? { target: "_blank", rel: "noopener" }
+                      : {}),
+                  })
+                );
+              }, []);
+
+              return null;
+            },
           }}
         />
       </BlogContentWrapper>
