ce5513f79b4c822e874c7e5fda1cfe6aa7d42c44
mass2527 <nzp01@naver.com>
2022-08-14 14:29:03 +0900

    feat: index.page에 SEO 컴포넌트 추가

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 
+import SEO from "@/components/SEO";
 import { Heading } from "@/components/Typography";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <SEO
+        title="필리 - 프론트엔드 엔지니어"
+        description="프론드엔드 기술과 지향하며 살고 싶은 가치관에 대해 기록합니다."
+      />
       <Heading as="h1">Hello.</Heading>
     </div>
   );
