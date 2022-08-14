a2932b18b392c6d65ca20a0ce45f8d5a3b8408f6
mass2527 <nzp01@naver.com>
2022-08-14 15:09:52 +0900

    feat: index.page 제목 변경

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -11,7 +11,10 @@ const Home: NextPage = () => {
         subTitle=""
         description="프론드엔드 기술과 지향하며 살고 싶은 가치관에 대해 기록합니다."
       />
-      <Heading as="h1">Hello.</Heading>
+      <Heading as="h1" fontSize={20}>
+        필리
+      </Heading>
+      <Heading fontSize={16}>프론트엔드 엔지니어</Heading>
     </div>
   );
 };
