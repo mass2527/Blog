891b4c31609be414177864ba1d14c58bab8857a2
mass2527 <nzp01@naver.com>
2022-09-24 19:00:48 +0900

    faet: Anchor 컴포넌트 개선 내부 링크(다른 페이지)일 경우 next/link 사용

--- src/pages/blog/components/Anchor.tsx
+++ src/pages/blog/components/Anchor.tsx
@@ -1,10 +1,35 @@
 import React, { ReactNode } from "react";
 
+import Link from "next/link";
+
+type LinkKind = "external" | "internal(same page)" | "internal(aother page)";
+
 function Anchor({ children, href }: { children?: ReactNode; href?: string }) {
-  const isInternalLink = href?.startsWith("#");
-  const attributes = isInternalLink
-    ? undefined
-    : { target: "_blank", rel: "noreferrer" };
+  if (!href) {
+    throw new Error(`<a>${children}</a>의 href가 입력되지 않았습니다.`);
+  }
+
+  let linkKind: LinkKind;
+  if (href.startsWith("https://")) {
+    linkKind = "external";
+  } else if (href.startsWith("#")) {
+    linkKind = "internal(same page)";
+  } else {
+    linkKind = "internal(aother page)";
+  }
+
+  if (linkKind === "internal(aother page)") {
+    return (
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
+    );
+  }
+
+  const attributes =
+    linkKind === "external"
+      ? { target: "_blank", rel: "noreferrer" }
+      : undefined;
 
   return (
     <a href={href} {...attributes}>
