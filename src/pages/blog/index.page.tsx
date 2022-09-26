db3d384a0624a99471c43b78691ddc550d37e9ca
mass2527 <nzp01@naver.com>
2022-09-26 23:10:24 +0900

    refactor: 게시 날짜, 리딩타임 TimeInfo 컴포넌트로 분리

--- src/pages/blog/index.page.tsx
+++ src/pages/blog/index.page.tsx
@@ -1,9 +1,8 @@
-import calculateReadingTime from "reading-time";
-
 import type { InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
 import SEO from "@/components/SEO";
+import TimeInfo from "@/components/TimeInfo";
 import { Heading, Text } from "@/components/Typography";
 import {
   blogFilePaths,
@@ -20,9 +19,6 @@ const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
       />
       <ul>
         {blogs.map(({ frontmatter, slug, matter }) => {
-          const { minutes } = calculateReadingTime(matter.content);
-          const readingTime = Math.round(minutes);
-
           return (
             <li key={frontmatter.title}>
               <Link href={`/blog/${slug}`}>
@@ -30,11 +26,10 @@ const Blog = ({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) => {
                   <article>
                     <Heading>{frontmatter.title}</Heading>
                     <Text>{frontmatter.summary}</Text>
-                    <time>{frontmatter.publishedAt.replace(/-/g, ". ")} —</time>
-                    <Text as="span" fontSize={14} color="mauve11">
-                      {" "}
-                      {readingTime}분
-                    </Text>
+                    <TimeInfo
+                      publishedAt={frontmatter.publishedAt}
+                      content={matter.content}
+                    />
                   </article>
                 </a>
               </Link>
