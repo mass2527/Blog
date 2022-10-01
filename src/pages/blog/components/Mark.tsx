8f03f770dc2ce5bbe877613481c200adfebd053d
mass2527 <nzp01@naver.com>
2022-10-01 18:03:56 +0900

    feat: Mark 컴포넌트 스타일 변경

--- src/pages/blog/components/Mark.tsx
+++ src/pages/blog/components/Mark.tsx
@@ -3,11 +3,8 @@ import styled from "styled-components";
 const Mark = styled.mark`
   display: block;
   text-align: center;
-  box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.plum3},
-    -15px 0 30px -15px ${({ theme }) => theme.colors.orange8},
-    0 0 30px -15px ${({ theme }) => theme.colors.pink8},
-    15px 0 30px -15px ${({ theme }) => theme.colors.violet8};
   background-color: transparent;
+  box-shadow: inset 0 0 0 2px ${({ theme }) => theme.colors.violet6};
   color: ${({ theme }) => theme.colors.mauve12};
   padding: ${({ theme }) => theme.spacers[24]};
   font-weight: ${({ theme }) => theme.fontWeights[500]};
