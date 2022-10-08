a796b7e6ac8b596cd3779aeaaba77e3ee9bc2e64
mass2527 <nzp01@naver.com>
2022-10-08 17:05:00 +0900

    feat: Flex 컴포넌트 props 추가

--- src/layouts/Flex.tsx
+++ src/layouts/Flex.tsx
@@ -1,7 +1,16 @@
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
+
+import { spacers } from "@/styles/theme";
 
 import { Box } from "./Box";
 
-export const Flex = styled(Box)`
+export const Flex = styled(Box)<{
+  justifyContent?: CSSProperties["justifyContent"];
+  alignItems?: CSSProperties["alignItems"];
+  gap?: keyof typeof spacers;
+}>`
   display: flex;
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
+  gap: ${({ theme, gap }) => gap && theme.spacers[gap]};
 `;
