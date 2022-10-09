bd06bc7f31ab65b66e5747bf0e49ee64d842818f
mass2527 <nzp01@naver.com>
2022-10-09 18:01:12 +0900

    feat: Flex 컴포넌트 flexDirection prop 추가

--- src/layouts/Flex.tsx
+++ src/layouts/Flex.tsx
@@ -5,11 +5,13 @@ import { spacers } from "@/styles/theme";
 import { Box } from "./Box";
 
 export const Flex = styled(Box)<{
+  flexDirection?: CSSProperties["flexDirection"];
   justifyContent?: CSSProperties["justifyContent"];
   alignItems?: CSSProperties["alignItems"];
   gap?: keyof typeof spacers;
 }>`
   display: flex;
+  flex-direction: ${({ flexDirection }) => flexDirection};
   justify-content: ${({ justifyContent }) => justifyContent};
   align-items: ${({ alignItems }) => alignItems};
   gap: ${({ theme, gap }) => gap && theme.spacers[gap]};
