fb7e997d2c770ddf06a030ef4dc9f1280db442d1
mass2527 <nzp01@naver.com>
2022-10-09 17:42:34 +0900

    feat: Card 컴포넌트 추가

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: ${({ theme }) => theme.colors.whiteA2};
+  box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.whiteA4};
+  backdrop-filter: blur(8px);
+  border-radius: ${({ theme }) => theme.radiuses[8]};
+  padding: ${({ theme }) => theme.spacers[16]};
+`;
+
+export default Card;
