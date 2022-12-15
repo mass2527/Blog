baf875090a88e852715c3ef8dac79e58b0b8a744
Dongho Kim <70563791+mass2527@users.noreply.github.com>
2022-12-15 20:34:47 +0900

    .github 폴더 제거 및 utteranc repo 이름 수정(#63)
    * chore: .github 폴더 제거
    
    * feat: utteranc repo 이름 수정
    
    Co-authored-by: philly <philly@phillyui-MacBookAir.local>

--- src/pages/blog/components/Comments.tsx
+++ src/pages/blog/components/Comments.tsx
@@ -6,7 +6,7 @@ function Comments() {
 
         const scriptElement = document.createElement('script');
         scriptElement.setAttribute('src', 'https://utteranc.es/client.js');
-        scriptElement.setAttribute('repo', 'mass2527/blog');
+        scriptElement.setAttribute('repo', 'mass2527/philly.im');
         scriptElement.setAttribute('issue-term', 'url');
         scriptElement.setAttribute('async', 'true');
         scriptElement.setAttribute('crossorigin', 'anonymous');
